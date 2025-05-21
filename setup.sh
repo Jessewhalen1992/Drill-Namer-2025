2f157b9db3dcfe5cf5e7fa5dad4f9d5beda72578
jesse Whalen <jesse_whalen@hotmail.com>
2025-05-21 10:03:04 -0300

    Fill in setup.sh with .NET SDK bootstrap

--- setup.sh
+++ setup.sh
@@ -1,1 +1,10 @@
-bash<br>#!/usr/bin/env bash<br>set -euxo pipefail<br>curl -SL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel LTS --install-dir "$HOME/dotnet"<br>echo 'export PATH=$HOME/dotnet:$PATH' >> "$BASH_ENV"<br>
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# Install latest LTS .NET SDK into $HOME/dotnet
+curl -SL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin \
+  --channel LTS \
+  --install-dir "$HOME/dotnet"
+
+# Make dotnet/msbuild visible to later steps
+echo 'export PATH=$HOME/dotnet:$PATH' >> "$BASH_ENV"
